b62bb1b2a5d29a60314eff4cf80cacbdd45dae3e
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2023-12-24 22:47:27 -0300

    OOP
    class with getter and setter.

---
+++ oop_class/src/oop_class/Oop_class.java
@@ -0,0 +1,15 @@
+package oop_class;
+
+public class Oop_class {
+
+    public static void main(String[] args) {
+        
+         VariableX myObj = new VariableX();
+         
+         System.out.println(VariableX.x);
+        
+         System.out.println(VariableX.getY());
+         
+    }
+    
+}
---
+++ oop_class/src/oop_class/VariableX.java
@@ -0,0 +1,17 @@
+package oop_class;
+
+public class VariableX {
+ 
+    public static int x = 5;
+    
+    private static float y = 7;
+
+    public static float getY() {
+        return y;
+    }
+
+    public static void setY(float y) {
+        VariableX.y = y;
+    }
+    
+}
