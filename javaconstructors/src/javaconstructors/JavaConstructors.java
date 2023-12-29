69ecb0584107e0f7844d14ea1bf3de604f990cba
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2023-12-28 22:43:29 -0300

    Constructors
    Class with method constructors

---
+++ javaconstructors/src/javaconstructors/JavaConstructors.java
@@ -0,0 +1,14 @@
+
+package javaconstructors;
+
+public class JavaConstructors {
+
+    public static void main(String[] args) {
+        
+        Main myObj = new Main(); // Create an object of class Main (This will call the constructor)
+    
+        System.out.println(myObj.x); // Print the value of x
+        
+    }
+    
+}
---
+++ javaconstructors/src/javaconstructors/Main.java
@@ -0,0 +1,15 @@
+
+package javaconstructors;
+
+public class Main {
+    
+    int x;  // Create a class attribute
+    
+    public Main() {
+    
+        x = 5 * 5;  // Set the initial value for the class attribute x
+  }
+    
+    
+    
+}
