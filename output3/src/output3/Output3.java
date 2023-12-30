33d43ffd8042c11ee3e44e62612b52aec26c7285
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2023-12-29 23:20:31 -0300

    Java Output Numbers
    mathematical calculations inside the println() method

---
+++ output3/src/output3/Output3.java
@@ -0,0 +1,14 @@
+
+package output3;
+
+public class Output3 {
+
+    public static void main(String[] args) {
+        
+        System.out.println(3 + 3);
+        
+        System.out.println(2 * 5);
+        
+    }
+    
+}
