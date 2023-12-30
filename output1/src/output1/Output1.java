9c96c4ced91b5082974f798e2f3f6343d7f40985
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2023-12-29 23:15:06 -0300

    Java Output / Print
    Print Text
    Double Quotes
    The Print() Method

---
+++ output1/src/output1/Output1.java
@@ -0,0 +1,15 @@
+
+package output1;
+
+public class Output1 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("This sentence will work!");
+        
+        System.out.print("Hello World! ");
+        System.out.print("I will print on the same line.");
+        
+    }
+    
+}
