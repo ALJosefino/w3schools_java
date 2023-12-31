91806eb67610c49f7bfcdcd6c86d3ffeb879e765
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2023-12-30 22:39:06 -0300

    Java Variables
    Create a variable called name of type String and assign it the value "John"

---
+++ javavariables/src/javavariables/JavaVariables.java
@@ -0,0 +1,13 @@
+
+package javavariables;
+
+public class JavaVariables {
+
+    public static void main(String[] args) {
+        
+        String name = "John";
+        System.out.println(name);
+        
+    }
+    
+}
