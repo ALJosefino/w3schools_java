c1d6d3e5c5d5ad28adc5f30f00fbe5ae49d84019
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2023-12-30 22:55:19 -0300

    Java Variables
    don't want others (or myself) to overwrite existing values, use the final keyword (this will declare the variable as "final" or "constant", which means unchangeable and read-only):

---
+++ javavariables4/src/javavariables4/JavaVariables4.java
@@ -0,0 +1,16 @@
+
+package javavariables4;
+
+public class JavaVariables4 {
+
+    public static void main(String[] args) {
+        
+        final int myNum = 15;
+        
+        //myNum = 20;   will generate an error: cannot assign a value to a final variable
+        
+        System.out.println(myNum);
+        
+    }
+    
+}
