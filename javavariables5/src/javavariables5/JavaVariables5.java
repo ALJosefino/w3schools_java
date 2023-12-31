e47b2f535b0cd09fd67d4aec97d86e6b5d6f7137
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2023-12-30 23:05:03 -0300

    Java Variable
    A demonstration of how to declare variables of other types

---
+++ javavariables5/src/javavariables5/JavaVariables5.java
@@ -0,0 +1,25 @@
+
+package javavariables5;
+
+public class JavaVariables5 {
+
+    public static void main(String[] args) {
+        
+        int myNum = 5;
+        float myFloatNum = 5.99f;
+        char myLetter = 'D';
+        boolean myBool = true;
+        String myText = "Hello";
+        
+        
+        System.out.println("int " + myNum);
+        System.out.println("flaot " + myFloatNum);
+        System.out.println("myLetter " + myLetter);
+        System.out.println("myBool " + myBool);
+        System.out.println("myText " + myText);
+       
+        
+        
+    }
+    
+}
