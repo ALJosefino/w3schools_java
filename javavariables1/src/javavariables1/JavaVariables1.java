5d2a1e75dcbf82f86492c2e7129ad9bff54a6700
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2023-12-30 22:40:58 -0300

    Create a variable called myNum of type int and assign it the value 15
    To create a variable that should store a number

---
+++ javavariables1/src/javavariables1/JavaVariables1.java
@@ -0,0 +1,13 @@
+
+package javavariables1;
+
+public class JavaVariables1 {
+
+    public static void main(String[] args) {
+        
+        int myNum = 15;
+        System.out.println(myNum);
+        
+    }
+    
+}
