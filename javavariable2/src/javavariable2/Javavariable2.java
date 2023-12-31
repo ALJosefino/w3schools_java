5556ee0f87be8c82c8532ebe7ded9966e0f74708
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2023-12-30 22:43:50 -0300

    Java Variables
    Declared a variable without assigning the value, and assigned the value later

---
+++ javavariable2/src/javavariable2/Javavariable2.java
@@ -0,0 +1,14 @@
+
+package javavariable2;
+
+public class Javavariable2 {
+
+    public static void main(String[] args) {
+        
+        int myNum;
+        myNum = 15;
+        System.out.println(myNum);
+        
+    }
+    
+}
