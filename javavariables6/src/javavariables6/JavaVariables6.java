7d3288e8ccbc6d29dad9d3cfc48ea29680759271
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2023-12-31 21:27:38 -0300

    Display Variables
    combined both text and a variable, used the + character, concatenated

---
+++ javavariables6/src/javavariables6/JavaVariables6.java
@@ -0,0 +1,13 @@
+
+package javavariables6;
+
+public class JavaVariables6 {
+
+    public static void main(String[] args) {
+        
+        String name = "André";
+        System.out.println("Hello " + name);
+        
+    }
+    
+}
---
+++ javavariables7/src/javavariables7/JavaVariables7.java
@@ -0,0 +1,15 @@
+
+package javavariables7;
+
+public class JavaVariables7 {
+
+    public static void main(String[] args) {
+        
+        String firstName = "André ";
+        String lastName = "Josefino ";
+        String fullName = firstName + lastName;
+        System.out.println(fullName);
+        
+    }
+    
+}
