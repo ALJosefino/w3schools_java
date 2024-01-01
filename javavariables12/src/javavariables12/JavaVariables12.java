da0a27538d592f81a090cc3d504034343175706e
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2023-12-31 22:00:50 -0300

    Java variables Identifiers
    All Java variables must be identified with unique names.
    
    These unique names are called identifiers.
    
    Identifiers can be short names (like x and y) or more descriptive names (age, sum, totalVolume).
    
    Note: It is recommended to use descriptive names in order to create understandable and maintainable code:
    
    The general rules for naming variables are:
    
    Names can contain letters, digits, underscores, and dollar signs
    Names must begin with a letter
    Names should start with a lowercase letter and it cannot contain whitespace
    Names can also begin with $ and _ (but we will not use it in this tutorial)
    Names are case sensitive ("myVar" and "myvar" are different variables)
    Reserved words (like Java keywords, such as int or boolean) cannot be used as names

---
+++ javavariables12/src/javavariables12/JavaVariables12.java
@@ -0,0 +1,20 @@
+
+package javavariables12;
+
+public class JavaVariables12 {
+
+    public static void main(String[] args) {
+        
+        // Good
+        int minutesPerHour = 60;
+
+        // OK, but not so easy to understand what m actually is
+        int m = 60;
+        
+        
+        System.out.println("minutesPerHour " + minutesPerHour);
+        
+        System.out.println("m " + m);
+    }
+    
+}
