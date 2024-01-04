2bf24d9b43cc2ef6eef5589a8ea8b9e92dfd5fc8
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2024-01-04 02:07:58 -0300

    Java Data Types
    variables in Java were specified data type

---
+++ javadatatypes/src/javadatatypes/JavaDataTypes.java
@@ -0,0 +1,26 @@
+
+package javadatatypes;
+
+public class JavaDataTypes {
+
+    public static void main(String[] args) {
+        
+        
+        int myNum = 5;               // Integer (whole number)
+        float myFloatNum = 5.99f;    // Floating point number
+        char myLetter = 'D';         // Character
+        boolean myBool = true;       // Boolean
+        String myText = "Hello";     // String
+        
+        System.out.println("myNum " + myNum);
+        System.out.println("myFloatNum " + myFloatNum);
+        System.out.println("myLetter " + myLetter);
+        System.out.println("myBool " + myBool);
+        System.out.println("myText " + myText);
+        
+        
+        
+        
+    }
+    
+}
