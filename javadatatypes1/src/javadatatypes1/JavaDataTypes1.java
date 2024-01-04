5a94c0ada64427da2ea4aec203b3260e7ef4a885
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2024-01-03 23:15:52 -0300

    Data types
    byte data type can store whole numbers from -128 to 127. This can be used instead of int or other integer types to save memory when you are certain that the value will be within -128 and 127

---
+++ javadatatypes1/src/javadatatypes1/JavaDataTypes1.java
@@ -0,0 +1,14 @@
+
+package javadatatypes1;
+
+public class JavaDataTypes1 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Data Type byte");
+        byte myNum = 100;
+        System.out.println(myNum);
+        
+    }
+    
+}
