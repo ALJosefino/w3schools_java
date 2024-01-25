f96f9228e9e3eff528dbc804d632b6eea1c60cfa
André L Josefino <ALJosefino@gmail.com>
2024-01-24 22:46:48 -0300

    Boolean Values
    A boolean type is declared with the boolean keyword and can only take the values true or false

---
+++ booleans/src/main/java/com/mycompany/booleans/Booleans.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.booleans;
+
+/**
+ *
+ * @author andre
+ */
+public class Booleans {
+
+    public static void main(String[] args) {
+        boolean isJavaFun = true;
+boolean isFishTasty = false;
+System.out.println(isJavaFun);     // Outputs true
+System.out.println(isFishTasty);   // Outputs false
+
+    }
+}
