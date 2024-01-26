600bdd06af9134c4b259045bd54db1d5f1b050f4
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2024-01-25 21:24:24 -0300

    Scientific Numbers
    A floating point number can also be a scientific number with an "e" to indicate the power of 10:

---
+++ datatypes/src/datatypes/DataTypes.java
@@ -0,0 +1,19 @@
+
+package datatypes;
+
+public class DataTypes {
+
+    public static void main(String[] args) {
+       
+        
+        float f1 = 35e3f;
+
+        double d1 = 12E4d;
+
+        System.out.println(f1);
+
+        System.out.println(d1);
+        
+    }
+    
+}
