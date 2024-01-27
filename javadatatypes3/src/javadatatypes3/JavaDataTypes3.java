ebb30b3fbcd4920769b0247afefa0d07d8f73268
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2024-01-26 23:38:50 -0300

    Characters
    The char data type is used to store a single character. The character must be surrounded by single quotes, like 'A' or 'c':

---
+++ javadatatypes3/src/javadatatypes3/JavaDataTypes3.java
@@ -0,0 +1,14 @@
+
+package javadatatypes3;
+
+public class JavaDataTypes3 {
+
+    public static void main(String[] args) {
+        
+        char myGrade = 'B';
+        
+        System.out.println(myGrade);
+        
+    }
+    
+}
