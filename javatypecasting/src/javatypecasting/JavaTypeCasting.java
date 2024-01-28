5b1dc37b5a1207e9343faf24af6ec0189d88e1b3
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2024-01-27 23:30:15 -0300

    Java Type Casting
    Java Type Casting
    Type casting is when you assign a value of one primitive data type to another type.
    
    In Java, there are two types of casting:
    
    Widening Casting (automatically) - converting a smaller type to a larger type size
    byte -> short -> char -> int -> long -> float -> double
    
    Narrowing Casting (manually) - converting a larger type to a smaller size type
    double -> float -> long -> int -> char -> short -> byte
    Widening Casting
    Widening casting is done automatically when passing a smaller size type to a larger size type:

---
+++ javatypecasting/src/javatypecasting/JavaTypeCasting.java
@@ -0,0 +1,18 @@
+
+package javatypecasting;
+
+public class JavaTypeCasting {
+
+    public static void main(String[] args) {
+        
+         int myInt = 9;
+         
+         double myDouble = myInt; // Automatic casting: int to double
+
+        System.out.println(myInt);      // Outputs 9
+    
+        System.out.println(myDouble);   // Outputs 9.0
+        
+    }
+    
+}
