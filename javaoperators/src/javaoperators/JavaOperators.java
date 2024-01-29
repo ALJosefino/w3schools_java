c143ab2212a59a206c1a0fd6fe7b9da11dcbec88
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2024-01-29 14:10:58 -0300

    Java Operators
    Operators are used to perform operations on variables and values.
    In the example below, we use the + operator to add together two values:

---
+++ javaoperators/src/javaoperators/JavaOperators.java
@@ -0,0 +1,14 @@
+
+package javaoperators;
+
+public class JavaOperators {
+
+    public static void main(String[] args) {
+       
+        int x = 100 + 50;
+        
+        System.out.println("x = " + x);
+        
+    }
+    
+}
