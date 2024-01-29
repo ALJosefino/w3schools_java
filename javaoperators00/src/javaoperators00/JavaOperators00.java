094a5b8d34bcd78416107b0e136c92edbc383529
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2024-01-29 14:16:40 -0300

    Java Operators
    Although the + operator is often used to add together two values, like in the example above, it can also be used to add together a variable and a value, or a variable and another variable:

---
+++ javaoperators00/src/javaoperators00/JavaOperators00.java
@@ -0,0 +1,22 @@
+
+package javaoperators00;
+
+public class JavaOperators00 {
+
+    public static void main(String[] args) {
+        
+        int sum1 = 100 + 50;        // 150 (100 + 50)
+
+        System.out.println("sum1 = 100 + 50 = " + sum1);
+        
+        int sum2 = sum1 + 250;      // 400 (150 + 250)
+
+        System.out.println("sum2 = sum1 + 250 = " + sum2);
+        
+        int sum3 = sum2 + sum2;     // 800 (400 + 400)
+        
+        System.out.println("sum3 = sum2 + sum2 = " + sum3);
+    
+    }
+    
+}
