ba84cc292b92a78a9a9b925f2425ed82e749109d
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2024-01-31 22:20:50 -0300

    Java Strings
    Strings are used for storing text.
    
    A String variable contains a collection of characters surrounded by double quotes:
    
    ExampleGet your own Java Server
    Create a variable of type String and assign it a value:

---
+++ javastrings/src/javastrings/JavaStrings.java
@@ -0,0 +1,14 @@
+
+package javastrings;
+
+public class JavaStrings {
+
+    public static void main(String[] args) {
+        
+        String greeting = "Hello";
+        
+        System.out.println(greeting);
+        
+    }
+    
+}
