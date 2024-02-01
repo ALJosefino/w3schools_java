453b0c92238fb724cfd5ad74d8afd10c9e3f7dc3
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2024-01-31 22:27:07 -0300

    More String Methods
    There are many string methods available, for example toUpperCase() and toLowerCase():
    
    Example

---
+++ javastrings00/src/javastrings00/JavaStrings00.java
@@ -0,0 +1,18 @@
+
+package javastrings00;
+
+public class JavaStrings00 {
+
+    public static void main(String[] args) {
+        
+        String txt = "Hello World";
+        
+        System.out.println(txt);
+
+        System.out.println(txt.toUpperCase());   // Outputs "HELLO WORLD"
+
+        System.out.println(txt.toLowerCase());   // Outputs "hello world"
+        
+    }
+    
+}
