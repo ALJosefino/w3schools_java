251ecb3dcb7082a93cd807bfe28930fbbbfa02ea
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2024-01-31 22:29:37 -0300

    Finding a Character in a String
    The indexOf() method returns the index (the position) of the first occurrence of a specified text in a string (including whitespace):
    
    Example

---
+++ javastrings02/src/javastrings02/JavaStrings02.java
@@ -0,0 +1,16 @@
+
+package javastrings02;
+
+public class JavaStrings02 {
+
+    public static void main(String[] args) {
+        
+        String txt = "Please locate where 'locate' occurs!";
+
+        System.out.println(txt);
+        
+        System.out.println(txt.indexOf("locate")); // Outputs 7
+        
+    }
+    
+}
