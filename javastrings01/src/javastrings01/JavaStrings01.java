43578f3c11b5955646a75605dcdae6aa7b19a3c2
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2024-01-31 22:24:40 -0300

    String Length
    A String in Java is actually an object, which contain methods that can perform certain operations on strings. For example, the length of a string can be found with the length() method:
    
    Example

---
+++ javastrings01/src/javastrings01/JavaStrings01.java
@@ -0,0 +1,16 @@
+
+package javastrings01;
+
+public class JavaStrings01 {
+
+    public static void main(String[] args) {
+        
+        String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        System.out.println(txt);
+        
+        System.out.println("The length of the txt string is: " + txt.length());
+        
+    }
+    
+}
