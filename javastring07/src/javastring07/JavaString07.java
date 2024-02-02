e7505bfdb91587539e59f30ba2047c95d22a94bd
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2024-02-01 21:27:41 -0300

    Java Numbers and Strings
    Adding Numbers and Strings
    
    If you add a number and a string, the result will be a string concatenation:

---
+++ javastring07/src/javastring07/JavaString07.java
@@ -0,0 +1,20 @@
+
+package javastring07;
+
+
+public class JavaString07 {
+
+    
+    public static void main(String[] args) {
+        
+        String x = "10";
+
+        int y = 20;
+
+        String z = x + y;  // z will be 1020 (a String)
+        
+        System.out.println(z);
+        
+    }
+    
+}
