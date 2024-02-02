410b8ffd76073d360e2d14763713de6f9b1a546a
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2024-02-01 21:09:40 -0300

    Java String Concatenation
    String Concatenation
    The + operator can be used between strings to combine them. This is called concatenation:

---
+++ javastrings03/src/javastrings03/JavaStrings03.java
@@ -0,0 +1,16 @@
+
+package javastrings03;
+
+public class JavaStrings03 {
+
+    public static void main(String[] args) {
+        
+        String firstName = "André";
+
+        String lastName = "Josefino";
+
+        System.out.println(firstName + " " + lastName);
+        
+    }
+    
+}
