475fd07b7fff739ce583380e8115422c2b9ea292
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2024-02-01 21:14:53 -0300

    Java String Concatenation
    String Concatenation
    You can also use the concat() method to concatenate two strings:

---
+++ javastrings04/src/javastring04/JavaStrings04.java
@@ -0,0 +1,16 @@
+
+package javastring04;
+
+public class JavaStrings04 {
+
+    public static void main(String[] args) {
+        
+        String firstName = "André ";
+
+        String lastName = "Josefino ";
+
+        System.out.println(firstName.concat(lastName));
+        
+    }
+    
+}
