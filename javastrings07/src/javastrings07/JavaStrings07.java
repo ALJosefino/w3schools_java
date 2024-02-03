61d4016421bb1bd063fdfbe83a9bc82f27f138d2
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2024-02-02 22:13:46 -0300

    Java Special Characters
    Strings - Special Characters
    Because strings must be written within quotes, Java will misunderstand this string, and generate an error:
    
    String txt = "We are the so-called "Vikings" from the north.";
    The solution to avoid this problem, is to use the backslash escape character.
    
    The backslash (\) escape character turns special characters into string characters:
    
    Escape character	Result	Description
    \'	'	Single quote
    \"	"	Double quote
    \\	\	Backslash
    The sequence \"  inserts a double quote in a string:

---
+++ javastrings07/src/javastrings07/JavaStrings07.java
@@ -0,0 +1,16 @@
+
+package javastrings07;
+
+public class JavaStrings07 {
+
+    public static void main(String[] args) {
+        
+        // String txt = "We are the so-called "Vikings" from the north.";
+        
+        String txt = "We are the so-called \"Vikings\" from the north.";
+        
+        System.out.println(txt);
+        
+    }
+    
+}
