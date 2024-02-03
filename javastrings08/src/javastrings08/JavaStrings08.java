95bda31a6b88471d1b0987f01c0322c73f840214
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2024-02-02 22:16:57 -0300

    Java Special Characters
    Strings - Special Characters
    
    The sequence \'  inserts a single quote in a string:

---
+++ javastrings08/src/javastrings08/JavaStrings08.java
@@ -0,0 +1,14 @@
+
+package javastrings08;
+
+public class JavaStrings08 {
+
+    public static void main(String[] args) {
+        
+         String txt = "It\'s alright.";
+         
+         System.out.println(txt);
+                
+    }
+    
+}
