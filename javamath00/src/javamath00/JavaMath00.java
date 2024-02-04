847147437e057fbb9acafd3e5a2704c0bbb83193
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2024-02-03 21:52:35 -0300

    Java Math
    The Java Math class has many methods that allows you to perform mathematical tasks on numbers.
    
    Math.max(x,y)
    The Math.max(x,y) method can be used to find the highest value of x and y:

---
+++ javamath00/src/javamath00/JavaMath00.java
@@ -0,0 +1,14 @@
+
+package javamath00;
+
+public class JavaMath00 {
+
+    public static void main(String[] args) {
+        
+        float m = Math.max(5, 10);
+        
+        System.out.println(m);
+        
+    }
+    
+}
