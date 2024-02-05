c85c205605653c58ae840dabc1490f4de3b9cf19
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2024-02-04 22:44:26 -0300

    Boolean Expression
    A Boolean expression returns a boolean value: true or false.
    
    This is useful to build logic, and find answers.
    
    For example, you can use a comparison operator, such as the greater than (>) operator, to find out if an expression (or a variable) is true or false

---
+++ javabooleans00/src/javabooleans00/JavaBooleans00.java
@@ -0,0 +1,22 @@
+
+package javabooleans00;
+
+public class JavaBooleans00 {
+
+    public static void main(String[] args) {
+        
+        int x = 10;
+
+        int y = 9;
+
+        System.out.println(x > y); // returns true, because 10 is higher than 9
+        
+        int x1 = (int) (Math.random() * 100);
+        
+        int y1 = (int) (Math.random() * 100);
+
+        System.out.println(x1 > y1);
+        
+    }
+    
+}
