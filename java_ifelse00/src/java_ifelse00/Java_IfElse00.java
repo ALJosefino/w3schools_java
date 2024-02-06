0fb37f087981858df1b86e857479b399ee632131
André L Josefino <ALJosefino@gmail.com>
2024-02-05 23:36:22 -0300

    Java If ... Else
    Java Conditions and If Statements
    You already know that Java supports the usual logical conditions from mathematics:
    
    Less than: a < b
    Less than or equal to: a <= b
    Greater than: a > b
    Greater than or equal to: a >= b
    Equal to a == b
    Not Equal to: a != b
    You can use these conditions to perform different actions for different decisions.
    
    Java has the following conditional statements:
    
    Use if to specify a block of code to be executed, if a specified condition is true
    Use else to specify a block of code to be executed, if the same condition is false
    Use else if to specify a new condition to test, if the first condition is false
    Use switch to specify many alternative blocks of code to be executed
    
    The if Statement
    
    We can also test variables:

---
+++ java_ifelse00/src/java_ifelse00/Java_IfElse00.java
@@ -0,0 +1,19 @@
+
+package java_ifelse00;
+
+public class Java_IfElse00 {
+
+    public static void main(String[] args) {
+        
+        int x = 20;
+
+        int y = 18;
+
+        if (x > y) {
+  
+            
+            System.out.println("x is greater than y");
+        
+        }
+    }
+}
