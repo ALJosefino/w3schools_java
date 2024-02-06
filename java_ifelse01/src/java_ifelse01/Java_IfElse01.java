b36c4cecb326d14ab97b8673000c61ec2f93a7bb
André L Josefino <ALJosefino@gmail.com>
2024-02-05 23:43:21 -0300

    Java If ... Else
    The else Statement
    Use the else statement to specify a block of code to be executed if the condition is false.
    
    Syntax
    if (condition) {
      // block of code to be executed if the condition is true
    } else {
      // block of code to be executed if the condition is false
    }
    
    Example explained
    In the example above, time (20) is greater than 18, so the condition is false. Because of this, we move on to the else condition and print to the screen "Good evening". If the time was less than 18, the program would print "Good day".

---
+++ java_ifelse01/src/java_ifelse01/Java_IfElse01.java
@@ -0,0 +1,24 @@
+
+package java_ifelse01;
+
+public class Java_IfElse01 {
+
+    public static void main(String[] args) {
+        
+        int time = 20;
+
+            if (time < 18) {
+  
+                System.out.println("Good day.");
+
+            } else {
+  
+                System.out.println("Good evening.");
+            
+            }
+
+        // Outputs "Good evening."
+        
+    }
+    
+}
