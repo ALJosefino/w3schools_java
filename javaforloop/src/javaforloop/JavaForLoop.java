9e94c65322815043267a4ace994c4c4408667de8
André L Josefino <ALJosefino@gmail.com>
2024-02-08 22:58:55 -0300

    Java For Loop
    Example explained
    Statement 1 sets a variable before the loop starts (int i = 0).
    
    Statement 2 defines the condition for the loop to run (i must be less than 6). If the condition is true, the loop will start over again, if it is false, the loop will end.
    
    Statement 3 increases a value (i++) each time the code block in the loop has been executed.

--- javaforloop/src/javaforloop/JavaForLoop.java
+++ javaforloop/src/javaforloop/JavaForLoop.java
@@ -4,7 +4,7 @@ public class JavaForLoop {
 
     public static void main(String[] args) {
     
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 6; i++) {
   
             System.out.println(i);
         
