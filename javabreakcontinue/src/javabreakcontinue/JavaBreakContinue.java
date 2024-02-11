a9e5f538a9f6af7ba1214fd64cb5387566693362
André L Josefino <ALJosefino@gmail.com>
2024-02-10 23:32:39 -0300

    Java Break and Continue
    Java Break
    You have already seen the break statement used in an earlier chapter of this tutorial. It was used to "jump out" of a switch statement.
    
    The break statement can also be used to jump out of a loop.
    
    This example stops the loop when i is equal to 4:

---
+++ javabreakcontinue/src/javabreakcontinue/JavaBreakContinue.java
@@ -0,0 +1,22 @@
+
+package javabreakcontinue;
+
+public class JavaBreakContinue {
+
+    public static void main(String[] args) {
+        
+        for (int i = 0; i < 10; i++) {
+  
+            if (i == 4) {
+    
+                break;
+  
+            }
+  
+            System.out.println(i);
+
+        }
+        
+    }
+    
+}
