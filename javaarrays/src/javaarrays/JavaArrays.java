b14a83cd2e60447d6b9426d018709b9fe0f1344d
André L Josefino <ALJosefino@gmail.com>
2024-02-11 23:53:22 -0300

    Java Arrays
    Arrays are used to store multiple values in a single variable, instead of declaring separate variables for each value.
    
    To declare an array, define the variable type with square brackets:

---
+++ javaarrays/src/javaarrays/JavaArrays.java
@@ -0,0 +1,17 @@
+
+package javaarrays;
+
+public class JavaArrays {
+
+    public static void main(String[] args) {
+        
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        
+        int[] myNum = {10, 20, 30, 40};
+        
+        System.out.println(cars[1]);
+        
+        
+    }
+    
+}
