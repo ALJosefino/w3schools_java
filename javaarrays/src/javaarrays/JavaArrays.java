89248876bf21d93f4b54f4a287fbd0f3cd652f16
André L Josefino <ALJosefino@gmail.com>
2024-02-12 23:51:51 -0300

    .

--- javaarrays/src/javaarrays/JavaArrays.java
+++ javaarrays/src/javaarrays/JavaArrays.java
@@ -5,12 +5,8 @@ public class JavaArrays {
 
     public static void main(String[] args) {
         
-        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
-        
-        int[] myNum = {10, 20, 30, 40};
-        
-        System.out.println(cars[1]);
-        
+        String[] cars;
+       
         
     }
     
