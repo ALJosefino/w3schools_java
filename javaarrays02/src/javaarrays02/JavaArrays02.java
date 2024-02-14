a874607e99c21404d6696efd079938da0527a140
André L Josefino <ALJosefino@gmail.com>
2024-02-13 23:48:29 -0300

    Access the Elements of an Array
    You can access an array element by referring to the index number.
    
    This statement accesses the value of the first element in cars:

---
+++ javaarrays02/src/javaarrays02/JavaArrays02.java
@@ -0,0 +1,18 @@
+
+package javaarrays02;
+
+
+public class JavaArrays02 {
+
+    
+    public static void main(String[] args) {
+        
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
+        System.out.println(cars[0]);
+
+        // Outputs Volvo
+        
+    }
+    
+}
