3514b695fc332eabc2e2876c26aa8c393e7f7f52
André L Josefino <ALJosefino@gmail.com>
2024-02-13 23:51:29 -0300

    Change an Array Element
    To change the value of a specific element, refer to the index number:

---
+++ javaarrays03/src/javaarrays03/JavaArrays03.java
@@ -0,0 +1,20 @@
+
+package javaarrays03;
+
+public class JavaArrays03 {
+
+    public static void main(String[] args) {
+        
+       String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
+       cars[0] = "Opel";
+
+       System.out.println(cars[0]);
+
+        // Now outputs Opel instead of Volvo
+        
+        
+        
+    }
+    
+}
