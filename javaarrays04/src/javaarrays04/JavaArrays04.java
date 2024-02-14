28cc6e7ceb02282aab763d1d3230be9d355a6415
André L Josefino <ALJosefino@gmail.com>
2024-02-13 23:53:34 -0300

    Array Length
    To find out how many elements an array has, use the length property:

---
+++ javaarrays04/src/javaarrays04/JavaArrays04.java
@@ -0,0 +1,16 @@
+
+package javaarrays04;
+
+public class JavaArrays04 {
+
+    public static void main(String[] args) {
+        
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
+        System.out.println(cars.length);
+
+        // Outputs 4
+        
+    }
+    
+}
