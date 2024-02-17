722250f3b50e9fe7d5f48b9470b1e6edee75f0bd
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2024-02-17 20:42:52 -0300

    Java Arrays - Java Multi-Dimensional Arrays
    Access Elements
    To access the elements of the myNumbers array, specify two indexes: one for the array, and one for the element inside that array. This example accesses the third element (2) in the second array (1) of myNumbers:
    
    Example
    
    Remember that: Array indexes start with 0: [0] is the first element. [1] is the second element, etc.

---
+++ javaarrays08/src/javaarrays08/JavaArrays08.java
@@ -0,0 +1,16 @@
+
+package javaarrays08;
+
+
+public class JavaArrays08 {
+
+    
+    public static void main(String[] args) {
+        
+        int[][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7} };
+
+        System.out.println(myNumbers[1][2]); // Outputs 7
+        
+    }
+    
+}
