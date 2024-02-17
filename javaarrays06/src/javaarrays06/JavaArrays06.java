95ace8674c02785f68a798044ae286308f12bfe4
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2024-02-16 22:52:52 -0300

    Java Arrays Loop
    Loop Through an Array with For-Each
    There is also a "for-each" loop, which is used exclusively to loop through elements in arrays:
    
    Syntax
    for (type variable : arrayname) {
      ...
    }
    The following example outputs all elements in the cars array, using a "for-each" loop:
    
    The example above can be read like this: for each String element (called i - as in index) in cars, print out the value of i.
    
    If you compare the for loop and for-each loop, you will see that the for-each method is easier to write, it does not require a counter (using the length property), and it is more readable.

---
+++ javaarrays06/src/javaarrays06/JavaArrays06.java
@@ -0,0 +1,18 @@
+
+package javaarrays06;
+
+public class JavaArrays06 {
+
+    public static void main(String[] args) {
+        
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
+        for (String i : cars) {
+  
+            System.out.println(i);
+
+        }
+        
+    }
+    
+}
