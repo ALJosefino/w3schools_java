3012f6b0686824b1d82d86dbe43478be1b071a40
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2024-02-17 20:40:39 -0300

    Java Arrays - Java Multi-Dimensional Arrays
    Multidimensional Arrays
    A multidimensional array is an array of arrays.
    
    Multidimensional arrays are useful when you want to store data as a tabular form, like a table with rows and columns.
    
    To create a two-dimensional array, add each array within its own set of curly braces:
    
    Example
    
    myNumbers is now an array with two arrays as its elements.

---
+++ javaarrays07/src/javaarrays07/JavaArrays07.java
@@ -0,0 +1,14 @@
+
+package javaarrays07;
+
+public class JavaArrays07 {
+
+    public static void main(String[] args) {
+        
+        int[][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7} };
+        
+        System.out.println(myNumbers[1][2]);
+        
+    }
+    
+}
