ce473d83211be3d3d961ceb766f17ff49edb1afb
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2024-02-18 23:34:28 -0300

    Java Methods
    A method is a block of code which only runs when it is called.
    
    You can pass data, known as parameters, into a method.
    
    Methods are used to perform certain actions, and they are also known as functions.
    
    Why use methods? To reuse code: define the code once, and use it many times.
    
    Create a Method
    A method must be declared within a class. It is defined with the name of the method, followed by parentheses (). Java provides some pre-defined methods, such as System.out.println(), but you can also create your own methods to perform certain actions:
    
    ExampleGet your own Java Server
    Create a method inside Main:
    
    Example Explained
    myMethod() is the name of the method
    static means that the method belongs to the Main class and not an object of the Main class. You will learn more about objects and how to access methods through objects later in this tutorial.
    void means that this method does not have a return value. You will learn more about return values later in this chapter

---
+++ javamethods/src/javamethods/JavaMethods.java
@@ -0,0 +1,17 @@
+
+package javamethods;
+
+public class JavaMethods {
+  
+    static void myMethod() {
+    
+            // code to be executed
+  
+    }
+    public static void main(String[] args) {
+        
+        
+ 
+  }
+ 
+}
