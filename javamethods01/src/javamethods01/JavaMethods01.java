3db6a680d7310fba15c10a63e137e36fe137de4c
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2024-02-19 11:12:37 -0300

    Java Methods - Call a Method
    A method is a block of code which only runs when it is called.
    
    You can pass data, known as parameters, into a method.
    
    Methods are used to perform certain actions, and they are also known as functions.
    
    Why use methods? To reuse code: define the code once, and use it many times
    
    Call a Method
    To call a method in Java, write the method's name followed by two parentheses () and a semicolon;
    
    In the following example, myMethod() is used to print a text (the action), when it is called:
    
    A method can also be called multiple times:
    
    In the next chapter, Method Parameters, you will learn how to pass data (parameters) into a method.

---
+++ javamethods01/src/javamethods01/JavaMethods01.java
@@ -0,0 +1,26 @@
+
+package javamethods01;
+
+public class JavaMethods01 {
+    
+  static void myMethod() {
+   
+      System.out.println("I just got executed!");
+
+    }
+    
+    public static void main(String[] args) {
+       
+    myMethod();
+    myMethod();
+    myMethod();
+      
+    }
+    
+// I just got executed!
+// I just got executed!
+// I just got executed!
+
+}
+
+
