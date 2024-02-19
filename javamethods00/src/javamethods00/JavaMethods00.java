e68ff64e5a9e975223e1e2ab338a8f22533330e3
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2024-02-19 11:08:02 -0300

    Java Methods - Call a Method
    To call a method in Java, write the method's name followed by two parentheses () and a semicolon;
    
    In the following example, myMethod() is used to print a text (the action), when it is called:
    
    Example
    Inside main, call the myMethod() method:

---
+++ javamethods00/src/javamethods00/JavaMethods00.java
@@ -0,0 +1,20 @@
+
+package javamethods00;
+
+public class JavaMethods00 {
+
+     static void myMethod() {
+    
+         System.out.println("I just got executed!");
+  
+     }
+    
+        public static void main(String[] args) {
+        
+            myMethod();
+        
+        }
+
+            // Outputs "I just got executed!"
+
+    }
