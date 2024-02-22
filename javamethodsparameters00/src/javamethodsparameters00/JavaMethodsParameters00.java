76b3b7b482773fc6f9e7ab086f64cac35dc6903a
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2024-02-21 22:34:17 -0300

    Java Method Parameters
    Multiple Parameters
    You can have as many parameters as you like:

---
+++ javamethodsparameters00/src/javamethodsparameters00/JavaMethodsParameters00.java
@@ -0,0 +1,26 @@
+
+package javamethodsparameters00;
+
+public class JavaMethodsParameters00 {
+
+      static void myMethod(String fname, int age) {
+    
+          System.out.println(fname + " is " + age);
+ 
+      }
+    
+            public static void main(String[] args) {
+       
+                    myMethod("Liam", 5);
+    
+                    myMethod("Jenny", 8);
+    
+                    myMethod("Anja", 31);
+  
+// Liam is 5
+// Jenny is 8
+// Anja is 31
+        
+    }
+    
+}
