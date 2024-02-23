d6c5c8341922530fd0b076e3d5c6e2a0b9bfbc05
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2024-02-22 22:46:15 -0300

    Java Method Parameters
    This example returns the sum of a method's two parameters:
    
    Example

---
+++ javamethodsparameters02/src/javamethodsparameters02/JavaMethodsParameters02.java
@@ -0,0 +1,19 @@
+
+package javamethodsparameters02;
+
+public class JavaMethodsParameters02 {
+
+    
+    static int myMethod(int x, int y) {
+    
+        return x + y;
+  }   
+        
+        public static void main(String[] args) {
+    
+            System.out.println(myMethod(5, 3));
+  }
+
+    // Outputs 8 (5 + 3)
+        
+    }
