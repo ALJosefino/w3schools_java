c5b60ab29bb9a92285033d0879fce7cf7e261e70
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2024-02-25 21:41:57 -0300

    Java Scope
    In Java, variables are only accessible inside the region they are created. This is called scope.
    
    Method Scope
    Variables declared directly inside a method are available anywhere in the method following the line of code in which they were declared:
    
    Example

---
+++ javascope/src/javascope/JavaScope.java
@@ -0,0 +1,18 @@
+
+package javascope;
+
+public class JavaScope {
+
+    public static void main(String[] args) {
+
+    // Code here CANNOT use x
+
+    int x = 100;
+
+    // Code here can use x
+    
+    System.out.println(x);
+        
+        }
+    
+}
