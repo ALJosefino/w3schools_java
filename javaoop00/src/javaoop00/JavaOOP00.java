464008c81537e943abc2580e840086efd2f62588
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2024-02-28 23:00:17 -0300

    Java Classes and Objects
    Multiple Objects
    You can create multiple objects of one class:
    
    Example
    Create two objects of Main:

---
+++ javaoop00/src/javaoop00/JavaOOP00.java
@@ -0,0 +1,18 @@
+
+package javaoop00;
+
+public class JavaOOP00 {
+
+    public static void main(String[] args) {
+       
+        Main myObj1 = new Main();  // Object 1
+    
+        Main myObj2 = new Main();  // Object 2
+    
+        System.out.println(myObj1.x);
+    
+        System.out.println(myObj2.x);
+        
+    }
+    
+}
---
+++ javaoop00/src/javaoop00/Main.java
@@ -0,0 +1,8 @@
+
+package javaoop00;
+
+public class Main {
+    
+      int x = 5;
+    
+}
