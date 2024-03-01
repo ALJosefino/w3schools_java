41190fdb3c9df45a9aa9aa7c864589f2c757eaee
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2024-02-29 23:40:15 -0300

    Modify Attributes
    If you don't want the ability to override existing values, declare the attribute as final:
    
    The final keyword is useful when you want a variable to always store the same value, like PI (3.14159...).
    
    The final keyword is called a "modifier". You will learn more about these in the Java Modifiers Chapter.

--- javaoop06/src/javaoop06/JavaOOP06.java
+++ javaoop06/src/javaoop06/JavaOOP06.java
@@ -7,7 +7,7 @@ public class JavaOOP06 {
         
          Main myObj = new Main();
     
-         myObj.x = 25; // will generate an error: cannot assign a value to a final variable
+         // myObj.x = 25;  will generate an error: cannot assign a value to a final variable
     
          System.out.println(myObj.x);
         
