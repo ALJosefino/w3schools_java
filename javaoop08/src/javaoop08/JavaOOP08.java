b80ca1724a6cde10a164e91c68fb2503ebfe6c3d
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2024-03-01 17:04:22 -0300

    Java Class Attributes
    Multiple Attributes
    You can specify as many attributes as you want:

---
+++ javaoop08/src/javaoop08/JavaOOP08.java
@@ -0,0 +1,16 @@
+
+package javaoop08;
+
+public class JavaOOP08 {
+
+    public static void main(String[] args) {
+       
+        Main myObj = new Main();
+    
+        System.out.println("Name: " + myObj.fname + " " + myObj.lname);
+    
+        System.out.println("Age: " + myObj.age);
+        
+    }
+    
+}
---
+++ javaoop08/src/javaoop08/Main.java
@@ -0,0 +1,12 @@
+
+package javaoop08;
+
+public class Main {
+    
+     String fname = "John";
+  
+     String lname = "Doe";
+  
+     int age = 24;
+    
+}
