ddef846788184b2957bc89e8708de853fc7ecf3c
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2024-03-02 22:39:49 -0300

    Java Class Methods
    myMethod() prints a text (the action), when it is called. To call a method, write the method's name followed by two parentheses () and a semicolon;
    
    Example
    Inside main, call myMethod():

---
+++ javaclassmethods00/src/javaclassmethods00/JavaClassMethods00.java
@@ -0,0 +1,21 @@
+
+package javaclassmethods00;
+
+public class JavaClassMethods00 {
+
+
+  static void myMethod() {
+  
+      System.out.println("Hello World!");
+ 
+  }
+
+  public static void main(String[] args) {
+    
+      myMethod();
+        
+        }
+  
+    }
+
+// Outputs "Hello World!"
---
+++ javaclassmethods00/src/javaclassmethods00/Main.java
@@ -0,0 +1,12 @@
+
+package javaclassmethods00;
+
+public class Main {
+    
+     public static void myMethod() {
+    
+         System.out.println("Hello World!");
+ 
+     }
+    
+}
