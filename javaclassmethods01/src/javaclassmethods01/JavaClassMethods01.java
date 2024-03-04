7bd2adac84b4b1b3074060a13dbd41bdf12a4c38
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2024-03-03 22:24:01 -0300

    Java Class Methods
    Static vs. Public
    You will often see Java programs that have either static or public attributes and methods.
    
    In the example above, we created a static method, which means that it can be accessed without creating an object of the class, unlike public, which can only be accessed by objects:
    
    Example
    An example to demonstrate the differences between static and public methods:

---
+++ javaclassmethods01/src/javaclassmethods01/JavaClassMethods01.java
@@ -0,0 +1,24 @@
+
+package javaclassmethods01;
+
+import static javaclassmethods01.Main.myStaticMethod;
+
+
+public class JavaClassMethods01 {
+
+    
+    
+    
+    public static void main(String[] args) {
+        
+        myStaticMethod(); // Call the static method
+    
+        // myPublicMethod(); This would compile an error
+
+        Main myObj = new Main(); // Create an object of Main
+    
+    myObj.myPublicMethod(); // Call the public method on the object
+        
+    }
+    
+}
