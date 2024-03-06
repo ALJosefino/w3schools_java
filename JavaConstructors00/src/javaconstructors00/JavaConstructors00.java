b30fa95ab90f7110c5024e2cc65e43ec89ca304d
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2024-03-05 23:41:45 -0300

    Java Constructors
    A constructor in Java is a special method that is used to initialize objects. The constructor is called when an object of a class is created. It can be used to set initial values for object attributes:
    
    ExampleGet your own Java Server
    Create a constructor:

---
+++ JavaConstructors00/src/javaconstructors00/JavaConstructors00.java
@@ -0,0 +1,14 @@
+
+package javaconstructors00;
+
+public class JavaConstructors00 {
+
+    public static void main(String[] args) {
+        
+        Main myObj = new Main(); // Create an object of class Main (This will call the constructor)
+    
+        System.out.println(myObj.x); // Print the value of x
+        
+    }
+    
+}
---
+++ JavaConstructors00/src/javaconstructors00/Main.java
@@ -0,0 +1,10 @@
+
+package javaconstructors00;
+
+// Create a Main class
+
+public class Main {
+
+    int x;  // Create a class attribute
+    
+}
