57048ddc205d979b6dbd7ba72ea3436367ab59ca
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2024-03-08 22:32:33 -0300

    .

--- JavaConstructors00/src/javaconstructors00/Main.java
+++ JavaConstructors00/src/javaconstructors00/Main.java
@@ -6,6 +6,11 @@ package javaconstructors00;
 public class Main {
 
     int x;  // Create a class attribute
- 
+
+    // Create a class constructor for the Main class
+  public Main() {
+    x = 5;  // Set the initial value for the class attribute x
+  }
+    
     
 }
