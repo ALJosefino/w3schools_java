cdf3acc05753b71cf5c32bd7b475c2a89e79899b
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2024-03-09 23:26:15 -0300

    Constructor Parameters
    Constructors can also take parameters, which is used to initialize attributes.
    
    The following example adds an int y parameter to the constructor. Inside the constructor we set x to y (x=y). When we call the constructor, we pass a parameter to the constructor (5), which will set the value of x to 5:

---
+++ javaconstructors01/src/javaconstructors01/JavaConstructors01.java
@@ -0,0 +1,15 @@
+
+package javaconstructors01;
+
+public class JavaConstructors01 {
+
+    public static void main(String[] args) {
+        
+        Main myObj = new Main(5);
+    
+        System.out.println(myObj.x);
+        
+        // Outputs 5
+    }
+    
+}
---
+++ javaconstructors01/src/javaconstructors01/Main.java
@@ -0,0 +1,14 @@
+
+package javaconstructors01;
+
+public class Main {
+  
+    int x;
+  
+        public Main(int y) {
+  
+      x = y;
+      
+      }
+  
+  }
