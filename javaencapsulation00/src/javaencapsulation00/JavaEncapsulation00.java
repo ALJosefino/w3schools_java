6ba5763566b44d632bfa4c4759e8dfa351a0be8c
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2024-03-16 22:35:57 -0300

    Java Encapsulation
    If the variable was declared as public, we would expect the following output:
    
    John
    However, as we try to access a private variable, we get an error:
    
    MyClass.java:4: error: name has private access in Person
        myObj.name = "John";
             ^
    MyClass.java:5: error: name has private access in Person
        System.out.println(myObj.name);
                      ^
    2 errors

---
+++ javaencapsulation00/src/javaencapsulation00/JavaEncapsulation00.java
@@ -0,0 +1,22 @@
+
+package javaencapsulation00;
+
+public class JavaEncapsulation00 {
+
+    public static void main(String[] args) {
+        
+        public class Main {
+  
+            public static void main(String[] args) {
+    
+      Person myObj = new Person();
+    
+    myObj.name = "John";  // error
+    
+    System.out.println(myObj.name); // error 
+  }
+}
+        
+    }
+    
+}
---
+++ javaencapsulation00/src/javaencapsulation00/Person.java
@@ -0,0 +1,19 @@
+
+package javaencapsulation00;
+
+public class Person {
+    
+     private String name; // private = restricted access
+
+  // Getter
+  public String getName() {
+    return name;
+  }
+
+  // Setter
+  public void setName(String newName) {
+    this.name = newName;
+ 
+  }
+    
+}
