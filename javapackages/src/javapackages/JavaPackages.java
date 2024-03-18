25130a3496eed5f3a2c77fe0f1af6a7dad054ac3
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2024-03-17 23:01:21 -0300

    Java Packages
    Java Packages & API
    A package in Java is used to group related classes. Think of it as a folder in a file directory. We use packages to avoid name conflicts, and to write a better maintainable code. Packages are divided into two categories:
    
    Built-in Packages (packages from the Java API)
    User-defined Packages (create your own packages)
    Built-in Packages
    The Java API is a library of prewritten classes, that are free to use, included in the Java Development Environment.
    
    The library contains components for managing input, database programming, and much much more. The complete list can be found at Oracles website: https://docs.oracle.com/javase/8/docs/api/.
    
    The library is divided into packages and classes. Meaning you can either import a single class (along with its methods and attributes), or a whole package that contain all the classes that belong to the specified package.
    
    To use a class or a package from the library, you need to use the import keyword:

---
+++ javapackages/src/javapackages/JavaPackages.java
@@ -0,0 +1,16 @@
+
+package javapackages;
+
+import package.name.Class;   // Import a single class
+
+import package.name.*;   // Import the whole package
+
+public class JavaPackages {
+
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+}
