41630b3f7d537cd0e7f12a17749afb88a9f9d1ed
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2024-03-18 22:32:05 -0300

    .

--- javapackages00/src/javapackages00/MyClass.java
+++ javapackages00/src/javapackages00/MyClass.java
@@ -1,13 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package javapackages00;
 
-/**
- *
- * @author andre
- */
+import java.util.Scanner;
+
 public class MyClass {
     
+    
+  public static void main(String[] args) {
+    
+      Scanner myObj = new Scanner(System.in);
+    
+    System.out.println("Enter username");
+
+    String userName = myObj.nextLine();
+    
+    System.out.println("Username is: " + userName);
+  }
 }
