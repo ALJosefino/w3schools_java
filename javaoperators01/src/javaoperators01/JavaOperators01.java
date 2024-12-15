ee6cfcdbb22a7844d889ef80cca78f3fc028d6f9
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2024-12-15 08:30:19 -0300

    Java Assignment Operators
    Assignment operators are used to assign values to variables.
    
    In the example below, we use the assignment operator (=) to assign the value 10 to a variable called x:

---
+++ javaoperators01/src/javaoperators01/JavaOperators01.java
@@ -0,0 +1,14 @@
+
+package javaoperators01;
+
+public class JavaOperators01 {
+
+    public static void main(String[] args) {
+        
+        int x = 10;
+
+        System.out.println("x = " + x);
+        
+    }
+    
+}
